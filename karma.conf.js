98a82e62ed45b49deae224223bb83aa2cbc0f677
wcastrov <wcastrov@psl.com.co>
2015-07-10 16:41:40 -0500

    added reports for coverage and junit xml

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,45 @@
-module.exports = function(config){
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    basePath : './',
+        basePath: './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/view*/**/*.js'
-    ],
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/components/**/*.js',
+            'app/view*/**/*.js'
+        ],
 
-    autoWatch : true,
+        autoWatch: true,
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+        browsers: ['Chrome'],
 
-    plugins : [
+        plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+            'karma-junit-reporter',
+            'karma-coverage'
+        ],
+        preprocessors: {
+            'app/components/**/*.js': 'coverage',
+            'app/view*/**/*.js': 'coverage'
+        },
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        },
 
-  });
+        reporters: ['coverage', 'junit'],
+
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
+        singleRun: false
+    });
 };
